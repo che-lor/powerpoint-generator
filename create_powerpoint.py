5dd5b68981f9aa3ed988e982b90c280ee181f9db
rol_ech <57928259+che-lor@users.noreply.github.com>
2020-06-16 15:03:14 -0400

    updates

--- create_powerpoint.py
+++ create_powerpoint.py
@@ -1,24 +1,31 @@
 #!/usr/bin/env python3
 
 from pptx import Presentation
-from pptx.util import Inches, Pt
+from pptx.dml.color import RGBColor
 import itertools
-import sys
 
+##################-Fill Out-###################
+###############################################
 file1 = "nameoffile"
 file2 = "nameoffile"
 file3 = "nameoffile"
+###############################################
+###############################################
+powerpoint = powerpoint_name + ".pptx"
+slide_master_template = "slide_master.pptx"
+save_message = "Powerpoint was created!"
+error_message = "Looks like there was an error :("
 
 with open(file1) as f1, open(file2) as f2, open(file3) as f3:
-	filelist1 = f1.read().splitlines()
-	filelist2 = f2.read().splitlines()
-	filelist3 = f3.read().splitlines()
-	lines = filelist1 + filelist2 + filelist3
+	file1_list = f1.read().splitlines()
+	file2_list = f2.read().splitlines()	
+	file3_list = f3.read().splitlines()
+	lines = list(file1_list + file2_list + file3_list)
 	f1.close()
 	f2.close()
 	f3.close()
-
-prs = Presentation()
+	
+prs = Presentation(slide_master_template)
 bullet_slide_layout = prs.slide_layouts[1]
 
 cycle = itertools.cycle(lines)
@@ -31,4 +38,9 @@ for i in lines:
 	tf = body_shape.text_frame
 	tf.text = next_line
 
-prs.save('test.pptx')
+save = prs.save(powerpoint)
+save
+if [ save ]:
+	print(save_message)
+else:
+	print(error_message)
